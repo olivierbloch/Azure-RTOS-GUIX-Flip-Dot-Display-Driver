712b6542d5dbe748692b76669f83ff6dffe0398a
olivierbloch <olivier.bloch@hotmail.fr>
2020-11-24 08:09:30 -0800

    Flipdot Guix driver clean up

--- flipdot_graphics_driver.c
+++ flipdot_graphics_driver.c
@@ -3,16 +3,18 @@
 #include "mt3620_lib/UART.h"
 #include "mt3620_lib/Print.h"
 
+// Typedef to simplify code reading
 typedef unsigned char byte;
 
-// 
+// Functions prototypes
 void send_to_flipdot(byte *messageToSend, unsigned int bytesToSend);
 void map_logical_display_to_physical_display(void);
-void send_to_display(void);
 
+// Serial port stuff
 #define FLIPDOT_UART MT3620_UNIT_ISU0
 UART *driver = NULL;
 
+// Flipdot hardware specifics
 typedef struct {
 	byte frameStart;
 	byte command;
@@ -24,11 +26,9 @@ typedef struct {
 const static byte cmd_sendToDisplay = 0x83;
 
 byte bFlipDisp[56];  // the logical display buffer.
-byte bOutBuffer[56];  // the vertical stripe reverse horizontal flip-disc display buffer.
+byte bOutBuffer[56];  // the vertical stripe reverse horizontal flip-disc display buffer (aka format expected by FlipDot controller).
 
-							 // need to shift
-const unsigned int mask[] = { 128,64,32,16,8,4,2,1 };
-
+const unsigned int mask[] = { 128,64,32,16,8,4,2,1 }; // Mask used to shift data
 const unsigned int getMask[] = { 128, 64, 32, 16, 8, 4, 2, 1 };
 const unsigned int setMask[] = { 64, 32, 16, 8, 4, 2, 1 };
 
@@ -36,59 +36,13 @@ flipFrame frame;
 int PixelPointer = 0;
 int PixelLength = 0;
 
-static void flipdot_buffer_toggle(GX_CANVAS *canvas, GX_RECTANGLE *dirty)
+void send_to_flipdot(byte *messageToSend, unsigned int bytesToSend)
 {
-	// Copy canvas to logical diplay buffer
-	memcpy(bFlipDisp, canvas->gx_canvas_memory, 56);
-
-	// Map to physical display
-	map_logical_display_to_physical_display();
-
-	// Send to flipdot
-	send_to_display();
+	UART_Write(driver, messageToSend, bytesToSend );
+	tx_thread_sleep(3);
 }
 
-UINT flipdot_graphics_driver_setup(GX_DISPLAY *display)
-{
-    // Init frame buffer
-	memset(bFlipDisp, 0x00, 56);
-
-	// setup the basic display frame
-	frame.frameStart = 0x80;
-	frame.command = cmd_sendToDisplay;
-	frame.frameEnd = 0x8f;
-	frame.address = 0x00; // top display
-
-	PixelPointer = 0;	// point to first pixel of the array.
-	PixelLength = 0;	// no string (yet).
-
-    // Init serial port
-	driver = UART_Open(FLIPDOT_UART, 57600, UART_PARITY_NONE, 1, NULL);
-
-    // perform standard function pointer setup
-    _gx_display_driver_monochrome_setup(display, GX_NULL, flipdot_buffer_toggle);
-
-	return GX_SUCCESS;
-}
-
-void send_to_display(void)
-{
-	// top display
-	frame.address = 0x00;
-	// copy the top display data.
-	memcpy(frame.data, bOutBuffer, 28);
-	// write the data
-	send_to_flipdot((byte*)&frame, sizeof(frame));
-
-	// bottom display
-	frame.address = 0x01;
-	// copy the top display data.
-	memcpy(frame.data, (bOutBuffer)+28, 28);
-	// write the data
-	send_to_flipdot((byte*)&frame, sizeof(frame));
-}
-
-// map the pixel x,y display to reverse vertical stripes.
+// map the pixel x,y display to reverse vertical stripes (format expected by the Flipdot controller).
 void map_logical_display_to_physical_display(void)
 {
 	int dispPtr = 0;
@@ -119,8 +73,52 @@ void map_logical_display_to_physical_display(void)
 	}
 }
 
-void send_to_flipdot(byte *messageToSend, unsigned int bytesToSend)
+static void flipdot_buffer_toggle(GX_CANVAS *canvas, GX_RECTANGLE *dirty)
 {
-	UART_Write(driver, messageToSend, bytesToSend );
-	tx_thread_sleep(3);
+	// Copy canvas to logical diplay buffer
+	memcpy(bFlipDisp, canvas->gx_canvas_memory, 56);
+
+	// Map to physical display
+	map_logical_display_to_physical_display();
+
+	// Send to flipdot
+	// top display
+	frame.address = 0x00;
+	// copy the top display data.
+	memcpy(frame.data, bOutBuffer, 28);
+	// write the data
+	send_to_flipdot((byte*)&frame, sizeof(frame));
+
+	// bottom display
+	frame.address = 0x01;
+	// copy the top display data.
+	memcpy(frame.data, (bOutBuffer)+28, 28);
+	// write the data
+	send_to_flipdot((byte*)&frame, sizeof(frame));
 }
+
+UINT flipdot_graphics_driver_setup(GX_DISPLAY *display)
+{
+    // Init frame buffer
+	memset(bFlipDisp, 0x00, 56);
+
+	// setup the basic display frame
+	frame.frameStart = 0x80;
+	frame.command = cmd_sendToDisplay;
+	frame.frameEnd = 0x8f;
+	frame.address = 0x00; // top display
+
+	PixelPointer = 0;	// point to first pixel of the array.
+	PixelLength = 0;	// no string (yet).
+
+    // Init serial port
+	driver = UART_Open(FLIPDOT_UART, 57600, UART_PARITY_NONE, 1, NULL);
+
+    // perform standard function pointer setup
+    _gx_display_driver_monochrome_setup(display, GX_NULL, flipdot_buffer_toggle);
+
+	return GX_SUCCESS;
+}
+
+
+
